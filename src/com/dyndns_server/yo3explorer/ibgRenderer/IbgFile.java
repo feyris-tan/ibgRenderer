9d8dde616ba550ab794847f75e94491f6f55c655
feyris-tan <feyris-tan@yo3explorer.dyndns-server.com>
2015-01-21 17:25:16 +0100

    Initial commit

---
+++ src/com/dyndns_server/yo3explorer/ibgRenderer/IbgFile.java
@@ -0,0 +1,39 @@
+package com.dyndns_server.yo3explorer.ibgRenderer;
+
+import java.io.*;
+
+/**
+ * Created by FT on 21.01.15.
+ * Taken from XY by Z
+ * Original Source:
+ */
+public class IbgFile
+{
+    public IbgFile(String s)
+    {
+
+    }
+
+    public IbgFile(File f)
+    {
+    }
+
+    public IbgFile(InputStream is)
+    {
+    }
+
+    public IbgFile(Reader reader)
+    {
+    }
+
+    public IbgFile(BufferedReader br)
+    {
+    }
+
+    BufferedReader br;
+
+    private void parseIbg()
+    {
+
+    }
+}
---
+++ src/com/dyndns_server/yo3explorer/ibgRenderer/Main.java
@@ -0,0 +1,13 @@
+package com.dyndns_server.yo3explorer.ibgRenderer;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        if (args.length != 1)
+        {
+            System.out.println("uage: java -jar ibgRenderer.jar myDisc.ibg");
+            return;
+        }
+    }
+}
